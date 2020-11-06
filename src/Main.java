fce614707d175624995753ee2f30dd0abfe000e8
Minh Hùng <73692771+zallero1@users.noreply.github.com>
2020-11-06 12:06:37 +0700

    update line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments  ,minh hhung
      */
     public static void main(String[] args) {
         System.out.println("Hướng dẫn SE1510 sử dụng Git");
